d19dcddf567e582b12e29ed37096d2bb74626ed4
braverokmc79 <braverokmc79@gmail.com>
2025-02-26 05:58:09 +0900

    ch3.프로바이더 & 서비스

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,88 +1,35 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
+  Param,
   Patch,
   Post,
-  Body,
-  Param,
-  HttpCode,
-  NotFoundException,
-  HttpStatus,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-  name: string;
-  character: string[];
-}
-
 @Controller('movie')
 export class AppController {
-  private idCounter = 5;
-
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: 'The Shawshank Redemption',
-      name: '스크린',
-      character: ['스크', '스 '],
-    },
-    {
-      id: 2,
-      title: 'The Godfather',
-      name: '미디안',
-      character: ['미디안', 'AA'],
-    },
-    {
-      id: 3,
-      title: 'Pulp Fiction',
-      name: '레나',
-      character: ['레나', '로버트 T. 로스'],
-    },
-    {
-      id: 4,
-      title: 'The Dark Knight',
-      name: '블 Panther',
-      character: ['블11 Panther', 'aa'],
-    },
-  ];
-
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies() {
-    console.log('Fetching all movies...');
-    return this.movies;
+    return this.appService.getManyMovies();
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    console.log(`Fetching movie with ID: ${id}`);
-    const movie = this.movies.find((m) => m.id === +id);
-    if (!movie) {
-      throw new NotFoundException(`Movie with ID ${id} not found`);
-    }
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.CREATED) // 201 상태 코드 반환
   postMovie(
     @Body() body: { name: string; character: string[] },
     @Body('title') title: string,
   ) {
-    console.log(`  ${title} Adding new movie: ${body.name}`);
-    const movie: Movie = {
-      id: this.idCounter++,
-      ...body,
-      title,
-    };
-
-    this.movies.push(movie);
-    return movie;
+    console.log(`Creating movie with title: ${title}`);
+    return this.appService.createMovie(title, body.name, body.character);
   }
 
   @Patch(':id')
@@ -91,32 +38,13 @@ export class AppController {
     @Body() body: { name?: string; character?: string[] },
     @Body('title') title: string,
   ) {
-    console.log(`Updating movie with ID: ${id}`);
-
-    const movie = this.movies.find((m) => m.id === +id);
-    if (!movie) {
-      throw new NotFoundException(`존재하지 않는 ${id} 입니다.`);
-    }
-
-    if (title) Object.assign(movie, { title });
-
-    if (body.name) Object.assign(movie, { name: body.name });
-    if (body.character) Object.assign(movie, { character: body.character });
-
-    return movie;
+    return this.appService.updateMovie(+id, title, body.name, body.character);
   }
 
   @Delete(':id')
-  //  @HttpCode(HttpStatus.NO_CONTENT) // 204 상태 코드 반환
   deleteMovie(@Param('id') id: string) {
     console.log(`Deleting movie with ID: ${id}`);
-
-    const movieIndex = this.movies.findIndex((m) => m.id === +id);
-    if (movieIndex === -1) {
-      throw new NotFoundException(`Movie with ID ${id} not found`);
-    }
-
-    this.movies.splice(movieIndex, 1);
+    this.appService.deleteMovie(+id);
 
     return {
       message: `Deleted movie with ID: ${id}`,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,83 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Movie {
+  id: number;
+  title: string;
+  name: string;
+  character: string[];
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private idCounter = 5;
+
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: 'The Shawshank Redemption',
+      name: '스크린',
+      character: ['스크', '스 '],
+    },
+    {
+      id: 2,
+      title: 'The Godfather',
+      name: '미디안',
+      character: ['미디안', 'AA'],
+    },
+    {
+      id: 3,
+      title: 'Pulp Fiction',
+      name: '레나',
+      character: ['레나', '로버트 T. 로스'],
+    },
+    {
+      id: 4,
+      title: 'The Dark Knight',
+      name: '블 Panther',
+      character: ['블11 Panther', 'aa'],
+    },
+  ];
+
+  getManyMovies(): Movie[] {
+    return this.movies;
+  }
+
+  getMovieById(id: number): Movie | undefined {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new Error(`No movie found with id ${id}`);
+    }
+    return movie;
+  }
+
+  createMovie(title: string, name: string, character: string[]) {
+    this.idCounter++;
+    this.movies.push({
+      id: this.idCounter,
+      title,
+      name,
+      character,
+    });
+    return this.getMovieById(this.idCounter);
+  }
+
+  updateMovie(id: number, title?: string, name?: string, character?: string[]) {
+    const movie = this.getMovieById(id);
+    if (!movie) {
+      throw new Error(`No movie found with id ${id}`);
+    }
+
+    if (title) Object.assign(movie, { title });
+    if (name) Object.assign(movie, { name });
+    if (character) Object.assign(movie, { character });
+    return movie;
+  }
+
+  deleteMovie(id: number) {
+    const movieIndex = this.movies.findIndex((m) => m.id === +id);
+    if (movieIndex === -1) {
+      throw new Error(`No movie found with id ${id}`);
+    }
+    this.movies.splice(movieIndex, 1);
   }
 }
