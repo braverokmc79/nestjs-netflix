4075b4e1fa79327bc198f257ab157f65eb6c1427
braverokmc79 <braverokmc79@gmail.com>
2025-06-14 13:57:27 +0900

    32.연결된 socket 관리 및  Room 들어가기

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+import { ChatRoom } from './entity/chat-room.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Chat } from './entity/chat.entity';
+import { User } from 'src/users/entity/user.entity';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    private readonly connectedClients =new Map<number, Socket>();
+
+    constructor(
+        @InjectRepository(ChatRoom)
+        private readonly chatRoomRepository : Repository<ChatRoom>,
+        @InjectRepository(Chat)
+        private readonly chatRepository : Repository<Chat>,
+        @InjectRepository(User)
+        private readonly userRepository : Repository<User>,
+    ){
+        
+    }
+
+
+
+    registerClient(userId: number, client: Socket) {
+        this.connectedClients.set(userId, client);
+    }
+
+    removeClient(userId: number) {
+        this.connectedClients.delete(userId);
+    }
+
+
+    async joinUserRooms(user: {sub:number}, client:Socket){
+        const chatRooms = await this.chatRepository.createQueryBuilder('chatRoom')
+        .innerJoin('chatRoom.users', 'user', 'user.id = :userId', { userId: user.sub })
+        .getMany();
+
+        
+        chatRooms.forEach((room) => {
+           void  client.join(room.id.toString());
+        });
+    }
+
+
+}
